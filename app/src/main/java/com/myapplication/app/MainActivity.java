5e27d8e8e20a45b63279f741a98353ece93ffadb
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-05-17 14:09:17 +0300

    created the buttons

--- app/src/main/java/com/myapplication/app/MainActivity.java
+++ app/src/main/java/com/myapplication/app/MainActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView resultTv, solutionTv;
+    MaterialButton buttonC, buttonBrackOpen, buttonBrackClose;
+    MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
+    MaterialButton button0,button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonDot;
 
 
     @Override
