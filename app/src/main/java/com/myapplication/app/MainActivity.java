9270cb4a70fb59bed9f4bc9a182160b3f2992552
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-05-17 16:07:35 +0300

    fixing the problems

--- app/src/main/java/com/myapplication/app/MainActivity.java
+++ app/src/main/java/com/myapplication/app/MainActivity.java
@@ -90,6 +90,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             context.setOptimizationLevel(-1);
             Scriptable scriptable = context.initSafeStandardObjects();
             String finalResult = context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+            if(finalResult.endsWith(".0")){
+                finalResult = finalResult.replace(".0", "");
+            }
             return finalResult;
         }catch(Exception e){
             return "Err";
